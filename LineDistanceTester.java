2f8f18b55da0dbaf1c35a3e6eb68ccee76e25f87
neerajarun2001 <55990778+neerajarun2001@users.noreply.github.com>
2019-10-03 20:43:45 -0400

    Add files via upload

---
+++ LineDistanceTester.java
@@ -0,0 +1,15 @@
+import java.awt.geom.Line2D;
+import java.awt.geom.Point2D;
+import java.awt.geom.Rectangle2D;
+public class LineDistanceTester {
+    public static void main(String[] args){
+        Line2D.Double line = new Line2D.Double(100, 100, 200, 200);
+        Point2D.Double point1 = new Point2D.Double(100, 100);
+        Point2D.Double point2 = new Point2D.Double(150, 150);
+        Point2D.Double point3 = new Point2D.Double(250, 50);
+
+        System.out.println(line.ptSegDist(point1));
+        System.out.println(line.ptSegDist(point2));
+        System.out.println(line.ptSegDist(point3));
+    }
+}
